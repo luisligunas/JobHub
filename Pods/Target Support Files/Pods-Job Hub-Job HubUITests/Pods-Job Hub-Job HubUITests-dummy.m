f6e9a6c1cd281c265da77d85b4043547451d3ca3
Luis Ligunas <lrtligunas@multisyscorp.com>
2021-01-08 08:51:49 +0800

    [.gitignore] Remove Pods and .xcworkspace files from .gitignore

---
+++ Pods/Target Support Files/Pods-Job Hub-Job HubUITests/Pods-Job Hub-Job HubUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Job_Hub_Job_HubUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Job_Hub_Job_HubUITests
+@end
---
+++ Pods/Target Support Files/Pods-Job HubTests/Pods-Job HubTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Job_HubTests : NSObject
+@end
+@implementation PodsDummy_Pods_Job_HubTests
+@end
